5eeeb963aa3b148cd366b058ffd6c00ade90a22b
Fernanda Constância <91707875+fernandaconstancia@users.noreply.github.com>
2022-10-19 21:08:19 -0300

    feat: create routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import { useNavigate } from "react-router-dom";
 import { Router } from "./Routes";
 import { GlobalStyle } from "./GlobalStyle";
 import { MenuNav } from "./components/MenuNav";
-import Logo from "./assets/logo.png";
+import Logo from "./assets/nameLogo.png";
+import Profile from "./assets/profile-pic.png";
+import { BsGithub, BsLinkedin, BsInstagram } from "react-icons/bs";
+import { Home } from "./pages/Home";
 
 export const App = () => {
   const nav = useNavigate();
 
   return (
-    <div>
+    <>
       <GlobalStyle />
-      <Router />
       <MenuNav />
-    </div>
+      <Router />
+      {/* <Home /> */}
+    </>
   );
 };
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+import { Contact } from "../pages/Contact";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route exact path="/" />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
       <Route path="/project" />
       <Route path="/resume" />
-      <Route path="/contact" />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 };
